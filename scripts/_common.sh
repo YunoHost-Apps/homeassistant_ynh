3b9abfafddb4600da55e2da3e328a48fcbdeb501
ewilly <ewilly@neuf.fr>
2022-01-07 21:26:59 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,25 +1,31 @@
-#
-# Common variables & functions
-#
+#!/bin/bash
+
+#=================================================
+# COMMON VARIABLES
+#=================================================
 
 # Release to install
-VERSION=2021.12.8
+app_version=2021.12.8
 
 # Package dependencies
-PKG_DEPENDENCIES="python3 python3-dev python3-venv python3-pip libffi-dev libssl-dev libjpeg-dev zlib1g-dev autoconf build-essential libopenjp2-7 libtiff5 libturbojpeg0 libmariadbclient-dev libmariadb-dev-compat"
+pkg_dependencies="python3 python3-dev python3-venv python3-pip libffi-dev libssl-dev libjpeg-dev zlib1g-dev autoconf build-essential libopenjp2-7 libtiff5 libturbojpeg0 libmariadbclient-dev libmariadb-dev-compat"
 
 # Requirements (Major.Minor.Patch)
 # PY_VERSION=$(curl -s "https://www.python.org/ftp/python/" | grep ">3.9" | tail -n1 | cut -d '/' -f 2 | cut -d '>' -f 2)
 # Pyhton 3.9.2 will be shiped with bullseye
-PY_REQUIRED_VERSION=3.9.2
+py_required_version=3.9.2
+
+#=================================================
+# PERSONAL HELPERS
+#=================================================
 
 # Create homeassistant user
 mynh_system_user_create () {
-    USER_GROUPS=""
-    [ $(getent group dialout) ] && USER_GROUPS="${USER_GROUPS} dialout"
-    [ $(getent group gpio) ] && USER_GROUPS="${USER_GROUPS} gpio"
-    [ $(getent group i2c) ] && USER_GROUPS="${USER_GROUPS} i2c"
-    ynh_system_user_create --username="$app" --groups="$USER_GROUPS" --home_dir="$data_path"
+    user_groups=""
+    [ $(getent group dialout) ] && user_groups="${user_groups} dialout"
+    [ $(getent group gpio) ] && user_groups="${user_groups} gpio"
+    [ $(getent group i2c) ] && user_groups="${user_groups} i2c"
+    ynh_system_user_create --username=$app --groups="$user_groups" --home_dir="$data_path"
 }
 
 
@@ -41,34 +47,40 @@ myynh_version_compare () {
 	# 0 -> A = B
 	# 1 -> A > B
 	# 2 -> A < B
-	if [[ $1 == $2 ]] ; then
+	if [[ $1 == $2 ]]
+	then
 		echo 0; return
 	fi
 	local IFS=.
 	local i ver1=($1) ver2=($2)
 	# fill empty fields in ver1 with zeros
-	for ((i=${#ver1[@]}; i<${#ver2[@]}; i++)) ; do
+	for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+	do
 		ver1[i]=0
 	done
-	for ((i=0; i<${#ver1[@]}; i++)) ; do
-		if [[ -z ${ver2[i]} ]] ; then
+	for ((i=0; i<${#ver1[@]}; i++))
+	do
+		if [[ -z ${ver2[i]} ]]
+		then
 			# fill empty fields in ver2 with zeros
 			ver2[i]=0
 		fi
-		if ((10#${ver1[i]} > 10#${ver2[i]})) ; then
+		if ((10#${ver1[i]} > 10#${ver2[i]}))
+		then
 			echo 1; return
 		fi
-		if ((10#${ver1[i]} < 10#${ver2[i]})) ; then
+		if ((10#${ver1[i]} < 10#${ver2[i]}))
+		then
 			echo 2; return
 		fi
 	done
 	echo 1; return
 }
 
-# Package dependencies
-# usage: myynh_install_dependencies --python="3.8.6"
+# Install specific python version
+# usage: myynh_install_python --python="3.8.6"
 # | arg: -p, --python=    - the python version to install
-myynh_install_dependencies () {
+myynh_install_python () {
 	# Declare an array to define the options of this helper.
 	local legacy_args=u
 	local -A args_array=( [p]=python= )
@@ -76,14 +88,12 @@ myynh_install_dependencies () {
 	# Manage arguments with getopts
 	ynh_handle_getopts_args "$@"
 	
-	# Install main dependencies from apt
-	ynh_install_app_dependencies "${PKG_DEPENDENCIES}"
-	
 	# Check python version from APT
 	local PY_APT_VERSION=$(python3 --version | cut -d ' ' -f 2)
 	
 	# Check existing built version of python in /usr/local/bin
-	if [ -e "/usr/local/bin/python${python:0:3}" ]; then
+	if [ -e "/usr/local/bin/python${python:0:3}" ]
+	then
 		local PY_BUILT_VERSION=$(/usr/local/bin/python${python:0:3} --version \
 			| cut -d ' ' -f 2)
 	else
@@ -91,20 +101,27 @@ myynh_install_dependencies () {
 	fi
 	
 	# Compare version
-	if [ $(myynh_version_compare $PY_APT_VERSION $python) -le 1 ]; then
+	if [ $(myynh_version_compare $PY_APT_VERSION $python) -le 1 ]
+	then
 		# APT >= Required
 		ynh_print_info --message="Using provided python3..."
+		
 		MY_PYTHON="python3"
+		
 	else
 		# Either python already built or to build 
-		if [ $(myynh_version_compare $PY_BUILT_VERSION $python) -le 1 ]; then
+		if [ $(myynh_version_compare $PY_BUILT_VERSION $python) -le 1 ]
+		then
 			# Built >= Required
 			ynh_print_info --message="Using already used python3 built version..."
+			
 			MY_PYTHON="/usr/local/bin/python${PY_BUILT_VERSION:0:3}"
+			
 		else
 			ynh_print_info --message="Installing additional dependencies to build python..."
-			PKG_DEPENDENCIES="${PKG_DEPENDENCIES} tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libbz2-dev libexpat1-dev liblzma-dev wget tar"
-			ynh_install_app_dependencies "${PKG_DEPENDENCIES}"
+			
+			pkg_dependencies="${pkg_dependencies} tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libbz2-dev libexpat1-dev liblzma-dev wget tar"
+			ynh_install_app_dependencies "${pkg_dependencies}"
 			# APT < Minimal & Actual < Minimal => Build & install Python into /usr/local/bin
 			ynh_print_info --message="Building python (may take a while)..."
 			# Store current direcotry 
@@ -130,7 +147,7 @@ myynh_install_dependencies () {
 		fi
 	fi
 	# Save python version in settings 
-	ynh_app_setting_set --app="$app" --key=python --value="$python"
+	ynh_app_setting_set --app=$app --key=python --value="$python"
 }
 
 # Install/Upgrade Homeassistant in virtual environement
@@ -145,7 +162,7 @@ myynh_install_homeassistant () {
 		&& echo 'install last version of mysqlclient' \
 			&& pip --cache-dir "$data_path/.cache" install --upgrade mysqlclient \
 		&& echo 'install Home Assistant' \
-			&& pip --cache-dir "$data_path/.cache" install --upgrade $app==$VERSION \
+			&& pip --cache-dir "$data_path/.cache" install --upgrade $app==$app_version \
 		"
 }
 
