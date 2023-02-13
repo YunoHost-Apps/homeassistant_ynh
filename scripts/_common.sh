e0ed24a3284652d0f7defb9952efe9b3cdf3e8d0
ewilly <ewilly@neuf.fr>
2023-02-13 22:41:19 +0100

    Update setuptools

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -142,8 +142,17 @@ myynh_install_homeassistant () {
 		# add pip
 		ynh_exec_as $app "$final_path/bin/python3" -m ensurepip
 		
-		# install last version of wheel, pip & mysqlclient
-		ynh_exec_as $app "$final_path/bin/pip3" --cache-dir "$data_path/.cache" install --upgrade wheel pip mysqlclient
+		# install last version of pip
+		ynh_exec_as $app "$final_path/bin/pip3" --cache-dir "$data_path/.cache" install --upgrade pip
+
+		# install last version of wheel
+		ynh_exec_as $app "$final_path/bin/pip3" --cache-dir "$data_path/.cache" install --upgrade wheel
+
+		# install last version of setuptools
+		ynh_exec_as $app "$final_path/bin/pip3" --cache-dir "$data_path/.cache" install --upgrade setuptools
+
+		# install last version of mysqlclient
+		ynh_exec_as $app "$final_path/bin/pip3" --cache-dir "$data_path/.cache" install --upgrade mysqlclient
 		
 		# install Home Assistant
 		ynh_exec_as $app "$final_path/bin/pip3" --cache-dir "$data_path/.cache" install --upgrade $app==$app_version
