04da7f1670c25f0d8b24b8e709fe6314e5c6637c
ewilly <ewilly@neuf.fr>
2023-10-07 10:32:40 +0200

    Remove old python directories before recreating them

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -149,6 +149,10 @@ myynh_upgrade_venv_directory () {
 	# Remove old python links before recreating them
 	find "$install_dir/bin/" -type l -name 'python*' -exec bash -c 'rm -f "$1"' _ {} \;
 	
+	# Remove old python directories before recreating them
+	find "$install_dir/lib/" -mindepth 1 -maxdepth 1 -type d -name "python*" -not -path "*/python${py_required_version%.*}" -exec bash -c 'rm -Rf "$1"' _ {} \;
+	find "$install_dir/include/site/" -mindepth 1 -maxdepth 1 -type d -name "python*" -not -path "*/python${py_required_version%.*}" -exec bash -c 'rm -Rf "$1"' _ {} \;
+	
 	# Upgrade the virtual environment directory
 	ynh_exec_as $app $py_app_version -m venv --upgrade "$install_dir"
 }
