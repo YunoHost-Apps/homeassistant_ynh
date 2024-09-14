ecb3f522963ef78538b6ba5a33584c0552f508c0
Sylvain <syl@thinkpad.localhost>
2024-09-14 21:11:31 +0200

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,9 +7,7 @@
 # App version
 ## yq is not a dependencie of yunohost package so tomlq command is not available
 ## (see https://github.com/YunoHost/yunohost/blob/dev/debian/control)
-app_version=$(cat ../manifest.toml 2>/dev/null \
-				| grep 'version = ' | cut -d '=' -f 2 \
-				| cut -d '~' -f 1 | tr -d ' "') #2024.2.5
+app_version=$(ynh_read_manifest "version" | cut -d '~' -f 1 ) #2024.2.5
 
 # Python required version
 ## jq is a dependencie of yunohost package
